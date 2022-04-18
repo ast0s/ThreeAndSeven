74b6e19ed973ab1ee463cc1a6e54d20fb84e9718
BulatNick <bulatnikov.mykyta@vu.cdu.edu.ua>
2022-04-18 12:15:58 +0300

    Добавьте файлы проекта.

---
+++ ThreeAndSeven/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreeAndSeven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+
+            CheckForBoth cfb = CheckForThree;
+            cfb += CheckForSeven;
+
+            for (int i = 0; i < array.Length; i++)
+                cfb(array[i], i);
+        }
+        public delegate void CheckForBoth(int number, int index);
+        static public void CheckForThree(int number, int index)
+        {
+            if (number % 3 == 0)
+                Console.WriteLine(index + ". -- " + number);
+        }
+        static public void CheckForSeven(int number, int index )
+        {
+            if (number % 7 == 0)
+                Console.WriteLine(index + ". -- " + number);
+        }
+    }
+}
